0ee00d2f8189a178ca2696569c9ddc1cbe0a9f7e
tarekul42 <tarekulrifat205@gmail.com>
2025-03-13 20:33:47 +0600

    blog added on the blogs page but without any kind of design

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React from "react";
 
-const Blog = ({blog}) => {
-    console.log(blog)
-    return (
-        <div>
-            
+const Blog = ({ blog }) => {
+  const {
+    title,
+    cover,
+    reading_time,
+    author_img,
+    author,
+    posted_date,
+    hashtags,
+  } = blog;
+  return (
+    <div>
+      <img src={cover} alt={`Cover picture of ${title}`} />
+      <div className="flex justify-between">
+        <div className="flex">
+          <img className="w-14" src={author_img} alt="" />
+          <div className="ml-6">
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
         </div>
-    );
+        <div>
+          <span>{reading_time}min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a href="">#{hash}</a>{" "}
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 export default Blog;
